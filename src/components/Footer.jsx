a4f8099ba1d90bf71c85856b1d853d226eaffe78
aaayush2604 <aayushfabwani2@gmail.com>
2024-09-28 17:24:51 +0530

    CSS Correction for Mobile 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
         </div>
         <div className='h-full w-full flex md:hidden bg-black justify-end relative items-end text-white'>
             <div className='bg-[#262727] w-4/5 h-4/5 absolute z-30 left-0'>
-            <div className='w-full h-full px-2 py-5 relative flex flex-col justify-center'>
+            <div className='w-full h-full px-2 py-5 relative flex flex-col justify-center font-FredokaRegular'>
                     <div className='text-[4.5vh]'>Let's Connect</div>
                     <div className='flex items-center mb-1'><FaPhoneAlt className='mr-4 size-4 sm:size-6'/> +91 9617637923</div>
                     <div className='flex items-center mb-1'><BiLogoGmail className='mr-4 size-4 sm:size-6'/> aayushfabwani2@gmail.com</div>
